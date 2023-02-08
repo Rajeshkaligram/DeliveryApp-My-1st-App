62b042f0e711553d9f573201ae3b977fa3a27abb
Rajeshkaligram <rajeshkaligram@gmail.com>
2023-02-09 02:45:18 +0530

    changes

--- App.js
+++ App.js
@@ -1,30 +1,92 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Title from './Screens/TitlePage';
 import LoginPage from './Screens/LoginPage';
 import Signup from './Screens/SignupPage';
 import HomePage from './Screens/Home';
+import Profile from './Screens/Profile';
+import Setting from './Screens/Setting';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {Image} from 'react-native';
 
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
+const TabBar = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {position: 'absolute'},
+        tabBarActiveTintColor: '#F1831B',
+        tabBarInactiveTintColor: '#7B7F85',
+        tabBarHideOnKeyboard: true,
+      }}>
+      <Tab.Screen
+        name="HomePage"
+        component={HomePage}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => {
+            return (
+              <Image
+                source={require('./Image/profile.png')}
+                style={{height: 30, width: 30}}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => {
+            return (
+              <Image
+                source={require('./Image/home.png')}
+                style={{height: 23, width: 23}}
+              />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
+const DrawerN = () => {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Home"
+      screenOptions={{headerShown: false}}>
+      <Drawer.Screen name="Home" component={TabBar} />
+      <Drawer.Screen name="Profile" component={Profile} />
+      <Drawer.Screen name="Setting" component={Setting} />
+    </Drawer.Navigator>
+  );
+};
 const App = () => {
-
-  const Stack = createStackNavigator();
-
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerBackTitle: true, headerTransparent: true, headerTitle: '' }}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
         <Stack.Screen name="Title" component={Title} />
-        <Stack.Screen name="Login" component={LoginPage} options={{ headerLeft: null }} />
+        <Stack.Screen
+          name="Login"
+          component={LoginPage}
+          options={{headerLeft: null}}
+        />
         <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="HomePage" component={HomePage} />
+        <Stack.Screen name="HomePage" component={DrawerN} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-
-
-export default App
-
+export default App;
--- Screens/SignupPage.js
+++ Screens/SignupPage.js
@@ -1,274 +1,288 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity, useColorScheme, StatusBar, Alert } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { TextInput } from 'react-native-gesture-handler';
-import BouncyCheckbox from "react-native-bouncy-checkbox";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  useColorScheme,
+  StatusBar,
+  Alert,
+} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {TextInput} from 'react-native-gesture-handler';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
 
 saveDetails = [];
 
-const SignupPage = ({ navigation }) => {
+const SignupPage = ({navigation}) => {
+  // useState for get Signup Data
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  const [check, setCheck] = useState(false);
 
-    // useState for get Signup Data
-    const [fullName, setFullName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const [check, setCheck] = useState(false);
-
-    // sign up data validation
-    const submit = () => {
-        if (fullName.length == 0 && email.length == 0 && password.length == 0) {
-            Alert.alert('Please filled all the fields');
-        }
-        else { checkData(fullName) }
-    };
-    const checkData = (e) => {
-        let rjx = /^[a-zA-Z" "]+$/;
-        if (!rjx.test(e)) {
-            Alert.alert("enter valid name");
-        }
-        else (checkData1(fullName))
+  // sign up data validation
+  const submit = () => {
+    if (fullName.length == 0 && email.length == 0 && password.length == 0) {
+      Alert.alert('Please filled all the fields');
+    } else {
+      checkData(fullName);
     }
-    const checkData1 = () => {
-        if (fullName.length == 0) {
-            Alert.alert('Please Enter Full Name');
-        }
-        else { checkData2(email) }
+  };
+  const checkData = e => {
+    let rjx = /^[a-zA-Z" "]+$/;
+    if (!rjx.test(e)) {
+      Alert.alert('enter valid name');
+    } else checkData1(fullName);
+  };
+  const checkData1 = () => {
+    if (fullName.length == 0) {
+      Alert.alert('Please Enter Full Name');
+    } else {
+      checkData2(email);
     }
-    const checkData2 = () => {
-        if (email.length == 0) {
-            Alert.alert('Please Enter valid Email Address');
-        }
-        else { checkData3(password) }
+  };
+  const checkData2 = () => {
+    if (email.length == 0) {
+      Alert.alert('Please Enter valid Email Address');
+    } else {
+      checkData3(password);
     }
-    const checkData3 = () => {
-        if (password.length == 0) {
-            Alert.alert('Please filled in Password');
-        }
-        else saveData();
-    }
+  };
+  const checkData3 = () => {
+    if (password.length == 0) {
+      Alert.alert('Please filled in Password');
+    } else saveData();
+  };
 
-    // store signup data
-    const saveData = async () => {
-        saveDetails.push({ name: fullName, email: email, password: password });
-        try {
-            await AsyncStorage.setItem('SAVE', JSON.stringify(saveDetails));
-            Alert.alert('Signup Successful');
-            console.log(saveDetails);
-            navigation.navigate('Login')
-        } catch (e) {
-        }
-    }
+  // store signup data
+  const saveData = async () => {
+    saveDetails.push({name: fullName, email: email, password: password});
+    try {
+      await AsyncStorage.setItem('SAVE', JSON.stringify(saveDetails));
+      Alert.alert('Signup Successful');
+      console.log(saveDetails);
+      navigation.navigate('Login');
+    } catch (e) {}
+  };
 
-    const isDarkMode = useColorScheme() === 'dark';
-    return (
-        <SafeAreaView style={styles.body}>
-            <StatusBar barStyle={isDarkMode ? 'dark-content' : 'dark-content'} />
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <SafeAreaView style={styles.body}>
+      <StatusBar barStyle={isDarkMode ? 'dark-content' : 'dark-content'} />
 
-            <View style={styles.containerLogo}>
-                <Image
-                    style={styles.logo}
-                    source={require('../Image/design1.png')} />
-            </View>
-            <View>
-                <View>
-                    <Text style={styles.login}>Create Account</Text>
-                </View>
+      <View style={styles.containerLogo}>
+        <Image style={styles.logo} source={require('../Image/design1.png')} />
+      </View>
+      <View>
+        <View>
+          <Text style={styles.login}>Create Account</Text>
+        </View>
 
-                <View style={styles.form}>
-                    <Image
-                        style={styles.name_logo}
-                        source={require('../Image/name.png')} />
-                    <TextInput
-                        style={styles.input_style}
-                        autoCapitalize='words'
-                        autoCorrect={false}
-                        placeholder='Full Name                                     '
-                        keyboardType='ascii-capable'
-                        value={fullName}
-                        onChangeText={(newData) => setFullName(newData)} />
-                </View>
+        <View style={styles.form}>
+          <Image
+            style={styles.name_logo}
+            source={require('../Image/name.png')}
+          />
+          <TextInput
+            style={styles.input_style}
+            autoCapitalize="words"
+            autoCorrect={false}
+            placeholder="Full Name                                     "
+            keyboardType="ascii-capable"
+            value={fullName}
+            onChangeText={newData => setFullName(newData)}
+          />
+        </View>
 
-                <View style={styles.form}>
-                    <Image
-                        style={styles.emailImage}
-                        source={require('../Image/EmailLogo.png')} />
-                    <TextInput
-                        style={styles.input_style}
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                        placeholder='Email                                       '
-                        keyboardType='email-address'
-                        value={email}
-                        onChangeText={(newData) => setEmail(newData)} />
-                </View>
+        <View style={styles.form}>
+          <Image
+            style={styles.emailImage}
+            source={require('../Image/EmailLogo.png')}
+          />
+          <TextInput
+            style={styles.input_style}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Email                                       "
+            keyboardType="email-address"
+            value={email}
+            onChangeText={newData => setEmail(newData)}
+          />
+        </View>
 
-                <View style={styles.form}>
-                    <Image
-                        style={styles.passImage}
-                        source={require('../Image/Password.png')} />
-                    <TextInput
-                        style={styles.input_style}
-                        maxLength={15}
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                        placeholder='Password                                    '
-                        textContentType='password'
-                        secureTextEntry={true}
-                        value={password}
-                        onChangeText={(newData) => setPassword(newData)} />
-                </View>
+        <View style={styles.form}>
+          <Image
+            style={styles.passImage}
+            source={require('../Image/Password.png')}
+          />
+          <TextInput
+            style={styles.input_style}
+            maxLength={15}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Password                                    "
+            textContentType="password"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={newData => setPassword(newData)}
+          />
+        </View>
 
-                <View style={styles.check_box}>
-                    <BouncyCheckbox
-                        size={25}
-                        fillColor="#F2A51D"
-                        textComponent={
-                            <Text style={{ color: '#7B7F85', paddingLeft: 10 }}>
-                                I have read and agree to the TC
-                            </Text>}
-                        isChecked={check}
-                        onPress={() => setCheck(!check)}
-                    />
-                </View>
+        <View style={styles.check_box}>
+          <BouncyCheckbox
+            size={25}
+            fillColor="#F2A51D"
+            textComponent={
+              <Text style={{color: '#7B7F85', paddingLeft: 10}}>
+                I have read and agree to the TC
+              </Text>
+            }
+            isChecked={check}
+            onPress={() => setCheck(!check)}
+          />
+        </View>
 
-
-                <View style={styles.button}>
-                    <View style={[styles.buttonStyle,
-                    {
-                        borderRadius: 20,
-                        backgroundColor: check ? '#F2A51D' : '#F5BC40'
-                    }
-                    ]}>
-                        <TouchableOpacity
-                            style={styles.loginButton}
-                            disabled={!check}
-                            onPress={() => submit()}>
-                            <Text style={styles.loginText}>
-                                SIGN UP
-                            </Text>
-                            <Image
-                                style={styles.buttonImage}
-                                source={require('../Image/RightArrow.png')} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                <View style={{ alignItems: 'center', marginTop: 68, flexDirection: 'row', justifyContent: 'center' }}>
-                    <Text style={{ color: '#7B7F85' }}>Already have a account?</Text>
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate('Login')}>
-                        <Text style={{ color: '#F2A51D', fontWeight: '800' }}>Sign in</Text>
-                    </TouchableOpacity>
-
-                </View>
-            </View>
-        </SafeAreaView>
-    )
-}
+        <View style={styles.button}>
+          <View
+            style={[
+              styles.buttonStyle,
+              {
+                borderRadius: 20,
+                backgroundColor: check ? '#F2A51D' : '#F5BC40',
+              },
+            ]}>
+            <TouchableOpacity
+              style={styles.loginButton}
+              disabled={!check}
+              onPress={() => submit()}>
+              <Text style={styles.loginText}>SIGN UP</Text>
+              <Image
+                style={styles.buttonImage}
+                source={require('../Image/RightArrow.png')}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View
+          style={{
+            alignItems: 'center',
+            marginTop: 68,
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+          <Text style={{color: '#7B7F85'}}>Already have a account?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+            <Text style={{color: '#F2A51D', fontWeight: '800'}}>Sign in</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    body: {
-        flex: 1,
-        margin: 0,
-    },
-    containerLogo: {
-        alignItems: 'flex-end',
-    },
-    login: {
-        fontSize: 34,
-        fontWeight: '900',
-        marginTop: 3,
-        marginLeft: 30,
-        marginBottom: 0,
-    },
-    logo: {
-        height: 150,
-        width: 150,
-    },
-    form: {
-        marginLeft: 30,
-        marginRight: 30,
-        marginTop: 25,
-        flexDirection: 'row',
-        alignItems: 'center',
+  body: {
+    flex: 1,
+    margin: 0,
+  },
+  containerLogo: {
+    alignItems: 'flex-end',
+  },
+  login: {
+    fontSize: 34,
+    fontWeight: '900',
+    marginTop: 3,
+    marginLeft: 30,
+    marginBottom: 0,
+  },
+  logo: {
+    height: 150,
+    width: 150,
+  },
+  form: {
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 25,
+    flexDirection: 'row',
+    alignItems: 'center',
 
-        borderBottomColor: '#CCD1DA',
-        borderTopColor: '#F1F3F7',
-        borderLeftColor: '#F1F3F7',
-        borderRightColor: '#CCD1DA',
-        borderRadius: 3,
-        borderWidth: 1,
-        height: 50,
-        shadowColor: '#ACB0B5',
-        shadowOffset: { width: 1, height: 4 },
-        shadowOpacity: 0.2,
-        shadowRadius: 3,
-        borderStartWidth: 2,
-    },
-    input_style: {
-        paddingHorizontal: 7,
-        paddingVertical: 10,
-        fontSize: 15,
-        flex: 1,
-    },
-    loginText: {
-        color: '#ffffff',
-        fontSize: 15,
-        fontWeight: '800',
-        paddingLeft: 7,
-    },
-    loginButton: {
-        padding: 10,
-        marginRight: 12,
-        width: 100,
-        flexDirection: 'row',
-    },
-    button: {
-        alignItems: 'flex-end',
-        marginRight: 30,
-        marginTop: 25,
-    },
-    name_logo: {
-        padding: 10,
-        margin: 5,
-        marginLeft: 7,
-        height: 20,
-        width: 20,
-        resizeMode: 'stretch',
-        alignItems: 'center',
-    },
-    emailImage: {
-        padding: 10,
-        height: 31,
-        width: 31,
-        resizeMode: 'stretch',
-        alignItems: 'center',
-    },
-    passImage: {
-        padding: 10,
-        margin: 5,
-        height: 10,
-        width: 10,
-        resizeMode: 'stretch',
-        alignItems: 'center',
-        marginRight: 5,
-    },
-    buttonImage: {
-        height: 17,
-        width: 17,
-    },
-    check_box: {
-        flexDirection: 'row',
-        marginTop: 25,
-        marginLeft: 30,
-    },
-    forgotText: {
-        color: '#F2A51D',
-    }
+    borderBottomColor: '#CCD1DA',
+    borderTopColor: '#F1F3F7',
+    borderLeftColor: '#F1F3F7',
+    borderRightColor: '#CCD1DA',
+    borderRadius: 3,
+    borderWidth: 1,
+    height: 50,
+    shadowColor: '#ACB0B5',
+    shadowOffset: {width: 1, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    borderStartWidth: 2,
+  },
+  input_style: {
+    paddingHorizontal: 7,
+    paddingVertical: 10,
+    fontSize: 15,
+    flex: 1,
+  },
+  loginText: {
+    color: '#ffffff',
+    fontSize: 15,
+    fontWeight: '800',
+    paddingLeft: 7,
+  },
+  loginButton: {
+    padding: 10,
+    marginRight: 12,
+    width: 100,
+    flexDirection: 'row',
+  },
+  button: {
+    alignItems: 'flex-end',
+    marginRight: 30,
+    marginTop: 25,
+  },
+  name_logo: {
+    padding: 10,
+    margin: 5,
+    marginLeft: 7,
+    height: 20,
+    width: 20,
+    resizeMode: 'stretch',
+    alignItems: 'center',
+  },
+  emailImage: {
+    padding: 10,
+    height: 31,
+    width: 31,
+    resizeMode: 'stretch',
+    alignItems: 'center',
+  },
+  passImage: {
+    padding: 10,
+    margin: 5,
+    height: 10,
+    width: 10,
+    resizeMode: 'stretch',
+    alignItems: 'center',
+    marginRight: 5,
+  },
+  buttonImage: {
+    height: 17,
+    width: 17,
+  },
+  check_box: {
+    flexDirection: 'row',
+    marginTop: 25,
+    marginLeft: 30,
+  },
+  forgotText: {
+    color: '#F2A51D',
+  },
 });
-
 
 export default SignupPage;
